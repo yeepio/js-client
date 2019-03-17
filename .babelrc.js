ecfbbfaf22431fb8e6b6ea6e1039b742fc0df2b4
Dimitrios C. Michalakos <dimitris@jmike.gr>
2019-03-17 17:00:47 +0200

    Upate babel to use current node with test suite

--- .babelrc.js
+++ .babelrc.js
@@ -2,12 +2,17 @@ module.exports = {
   presets: [
     [
       '@babel/preset-env',
-      {
-        targets: {
-          browsers: ['last 2 versions', 'ie >= 11'],
-        },
-        modules: process.env.NODE_ENV === 'test' && 'commonjs',
-      },
+      process.env.NODE_ENV === 'test'
+        ? {
+            targets: {
+              node: 'current',
+            },
+          }
+        : {
+            targets: {
+              browsers: ['last 2 versions', 'ie >= 11'],
+            },
+          },
     ],
   ],
   plugins: [['@babel/plugin-proposal-class-properties', { loose: true }]],
